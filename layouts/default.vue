f742a37bbef5a190c2d90bd7f1942b1e43eca7c7
dygapp <dygapp@qq.com>
2019-04-04 10:06:20 +0800

    增加浏览器版本检查

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <banner />
-    <navx />
-    <nuxt />
-    <feet />
+    <div class="alert" style="display: none;" v-show="showAlert">
+      <span>您使用的浏览器版本过低，可能会影响网站浏览效果，建议使用IE10以上浏览器或最新版谷歌浏览器。</span>
+    </div>
+    <div class="header">
+      <banner />
+      <navx />
+    </div>
+    <div class="content">
+      <nuxt />
+    </div>
+    <div class="footer">
+      <feet />
+    </div>
   </div>
 </template>
 
@@ -11,12 +20,30 @@
 import banner from '~/components/layout/banner';
 import navx from '~/components/layout/navx';
 import feet from '~/components/layout/feet';
+import { detect } from 'detect-browser';
+import semver from 'semver';
 
 export default {
   components: {
     banner,
     navx,
     feet,
+  },
+  data() {
+    return {
+      showAlert: false,
+    };
+  },
+  mounted() {
+    // 检查浏览器版本
+    if (process.browser) {
+      const browser = detect();
+      console.log(browser);
+      if(browser.name === 'ie' && semver.lt(browser.version, '10.0.0')) {
+        this.showAlert = true;
+        console.warn('您使用的浏览器版本过低，可能会影响网站浏览效果，请更换新版本的浏览器，建议使用最新版谷歌浏览器。')
+      }
+    }
   },
 };
 </script>
@@ -67,5 +94,26 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+</style>
+<style scoped>
+.content {
+  margin: 2em 0;
+}
+.content > div {
+  min-height: 400px;
+}
+
+.alert {
+  text-align: center;
+  line-height: 24px;
+  background: lightyellow;
+  position: fixed;
+  top: 0;
+  z-index: 99999;
+  width: 100%;  
+}
+.alert span {
+  display: inline;
 }
 </style>
